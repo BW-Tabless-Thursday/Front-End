b5f1f617319c3a038503b6529776f74dafd0666f
wtrawlings <rawlings.warren@gmail.com>
2019-11-17 22:04:06 -0500

    started on CreateTabs

---
+++ tabless-thursday/src/components/Categories/ListOfCategories.js
@@ -0,0 +1,2 @@
+//Look at my other react items. See if we can render the same way
+//as other projects using .map to create list of categories
--- tabless-thursday/src/components/Tabs/ListOfTabs.js
+++ tabless-thursday/src/components/Tabs/ListOfTabs.js
@@ -2,5 +2,5 @@
 //this will be a mapped array of objects like we normally work with.
 //ListOfLinks will be set by Category selected
 //You select a Category and it generates the ListOfLinks
-//Show your list of individual links in an expansion pannel
+//Show your list of individual links in an EXPANSION PANNEL
 //please reference the material ui for this expansion pannel function 
