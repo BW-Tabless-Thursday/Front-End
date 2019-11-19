87cb04bbaadc213dc2e50188a5484ec2bebbbc97
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-11-18 18:41:21 -0600

    working on getting the tabs

--- tabless-thursday/src/components/Tabs/ListOfTabs.js
+++ tabless-thursday/src/components/Tabs/ListOfTabs.js
@@ -4,3 +4,36 @@
 //You select a Category and it generates the ListOfLinks
 //Show your list of individual links in an EXPANSION PANNEL
 //please reference the material ui for this expansion pannel function 
+
+import React, { useState, useEffect } from "react";
+import api from "../../utils/api";
+import TabPreview from "./Tab";
+
+export default function ListOfTabs(props) {
+	const [tabs, setTabs] = useState([])
+
+	useEffect(() => {
+		api()
+        //   .get(`/tabs/${user_id}`)
+          .get(`tabs/${props.path.params.user_id}`)
+		  .then(response => {
+              console.log(response.data.tabs)
+			setTabs(response.data.tabs);
+		  })
+		  .catch(error => {
+			console.log(error);
+		  });
+	  }, []);
+
+	return (
+		<div>
+            <h3>My tabs</h3>
+			<div>
+				{tabs &&
+					tabs.map(tab => (
+						<TabPreview key={tab.id} tab={tab}/>
+				))}
+			</div>
+        </div>
+	)
+}
--- tabless-thursday/src/components/Tabs/Tab.js
+++ tabless-thursday/src/components/Tabs/Tab.js
@@ -2,29 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import api from "../../utils/api";
 
-export default function TabPreview(){
-    const [tabs, setTab] = useState({
-        "name" : "",
-        "url" : "",
-        "notes" : "",
-        "id" : ""
-    })
+export default function TabPreview(props){
+    // const [tabs, setTab] = useState({
+    //     "name" : "",
+    //     "url" : "",
+    //     "notes" : "",
+    //     "id" : ""
+    // })
 
-    useEffect(() => {
-        api()
-            .get("/tabs/{{user_id}}")
-            .then((result) => {
-                setTab(result.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }, [])
+    // useEffect(() => {
+    //     api()
+    //         .get("/tabs/{{user_id}}")
+    //         .then((result) => {
+    //             setTab(result.data)
+    //         })
+    //         .catch((error) => {
+    //             console.log(error)
+    //         })
+    // }, [])
 
     return(
         <div>
-            <h1>My tabs</h1>
-            <p>{tabs.name.toString()}</p>
+            <h3>{props.tab.name}</h3>
         </div>
     )
 }
