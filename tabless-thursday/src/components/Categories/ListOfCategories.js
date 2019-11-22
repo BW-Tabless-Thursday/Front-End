068e606f7dd499fbb8c3bb53f7eef2fb650e7d5b
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-11-21 18:41:16 -0600

    working on categories

--- tabless-thursday/src/components/Categories/ListOfCategories.js
+++ tabless-thursday/src/components/Categories/ListOfCategories.js
@@ -1,2 +1,37 @@
 //Look at my other react items. See if we can render the same way
 //as other projects using .map to create list of categories
+
+
+import React, { useState, useEffect } from "react";
+import api from "../../utils/api";
+
+export default function ListOfCategories(){
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        api()
+            .get("/tabs/categories")
+            .then((result) => {
+				console.log(result.data)
+				setCategories(result.data)
+				
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+	}, [])
+
+    return(
+        <div>
+
+            {categories &&
+                categories.map(category => (
+                    <div>
+                        <h1>{category.name}</h1>
+                    </div>
+            ))}
+
+        </div>
+    )
+}
